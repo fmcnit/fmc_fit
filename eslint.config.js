34cf5b5aae977c4a1c643c9001c2ab851d3c3d59
fmcnit <fmcnit@gmail.com>
2026-03-08 16:13:55 -0300

    chore: prettier eslint

---
+++ eslint.config.js
@@ -0,0 +1,14 @@
+import js from "@eslint/js";
+import eslintConfigPrettier from "eslint-config-prettier";
+import { defineConfig } from "eslint/config";
+import tseslint from "typescript-eslint";
+
+export default defineConfig([
+  {
+    files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+  },
+  tseslint.configs.recommended,
+  eslintConfigPrettier,
+]);
