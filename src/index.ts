16c1de2433571f5b97c3fecc9ee02c6ff0903513
fmcnit <fmcnit@gmail.com>
2026-03-15 11:28:50 -0300

    chore: fastify

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,67 @@
-console.log("Hello World");
+import { z } from 'zod/v4';
+import "dotenv/config";
+
+import Fastify from 'fastify'
+const app = Fastify({
+  logger: true
+})
+import { serializerCompiler, validatorCompiler, ZodTypeProvider, jsonSchemaTransform } from 'fastify-type-provider-zod';
+import fastifySwagger from '@fastify/swagger';
+
+
+
+
+app.setValidatorCompiler(validatorCompiler);
+app.setSerializerCompiler(serializerCompiler);
+
+await app.register(fastifySwagger, {
+  openapi: {
+    info: {
+      title: 'BootCamp Treinos',
+      description: 'Sample backend service',
+      version: '1.0.0',
+    },
+    servers: [
+      {
+        description: "Localhost",
+        url: "http://localhost:8081",
+      }
+    ],
+  },
+  transform: jsonSchemaTransform,
+
+});
+
+await app.register(fastifySwagger, {
+  routePrefix: '/docs',
+}); 
+
+
+app.withTypeProvider<ZodTypeProvider>().route({
+  method: 'GET',
+  url: '/',
+ 
+  schema: {
+    description: 'Hello World',
+    tags: ['hello'],
+    response: {
+      200: z.object({
+        message: z.string(),
+      }),
+
+    },
+  },
+  handler: () => {
+    return { 
+      message: 'Hello World' 
+    }
+  },
+});
+
+
+try {
+  await app.listen({ port: Number(process.env.PORT) || 8081})
+} catch (err) {
+  app.log.error(err)
+  process.exit(1)
+}
